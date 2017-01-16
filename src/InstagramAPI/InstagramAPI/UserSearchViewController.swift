6da1cfce0594f4b5a19d8bdb972e3bdb84565f2a
Sasha Kid <kidpost@gmail.com>
2017-01-16 18:35:10 +0300

    user search ui is in progress

---
+++ src/InstagramAPI/InstagramAPI/UserSearchViewController.swift
@@ -0,0 +1,63 @@
+//
+//  UserSearchViewController.swift
+//  InstagramAPI
+//
+//  Created by Yakovlev, Alexander on 1/10/17.
+//  Copyright © 2017 ConceptOffice. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchViewController: UIViewController {
+    
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    @IBOutlet fileprivate weak var searchBar: UISearchBar!
+    fileprivate var dataSource: [InstagramUser?] = []
+    fileprivate var searchActive : Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension UserSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserSearchCell") as! UserSearchCell;
+        return cell;
+    }
+}
+
+extension UserSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        InstagramManager.shared.userService.fetchUsers(searchText: searchText) { (users, error) in
+            guard let users = users else {
+                return
+            }
+            self.dataSource = users
+            self.tableView.reloadData()
+        }
+    }
+}
--- src/InstagramAPI/InstagramAPI/UserViewController.swift
+++ src/InstagramAPI/InstagramAPI/UserViewController.swift
@@ -11,7 +11,7 @@ import AlamofireImage
 import Alamofire
 
 class UserViewController: UIViewController {
-
+    
     var userID: String?
     
     @IBOutlet private weak var avatarImageView: UIImageView!
@@ -26,43 +26,40 @@ class UserViewController: UIViewController {
         super.viewDidLoad()
         
         InstagramManager.shared.userService.fetchUser(userId: userID) { (user, error) in
-            if error == nil {
-                if user == user {
-                    if let url = user?.profilePictureURL?.absoluteString {
-                        Alamofire.request(url).responseImage { response in
-                            //debugPrint(response.result)
-                            if let image = response.result.value {
-                                self.avatarImageView.image = image
-                            }
-                        }
-                    }
-                    
-                    if let fullName = user?.fullName {
-                        self.title = fullName
-                    }
-                    if let nickname = user?.username {
-                        self.nicknameLabel.text = nickname
-                    }
-                    if let posts = user?.counts?.media {
-                        self.postsLabel.text = "\(posts) posts"
-                    }
-                    if let followers = user?.counts?.followedBy {
-                        self.followersLabel.text = "\(followers) followers"
-                    }
-                    if let following = user?.counts?.follows {
-                        self.followingLabel.text = "\(following) following"
-                    }
-                    if let bio = user?.bio {
-                        self.bioLabel.text = bio
-                    }
-                    if let website = user?.website?.absoluteString {
-                        self.websiteLabel.text = website
+            guard let user = user, error == nil else {
+                return
+            }
+            
+            if let url = user.profilePictureURL?.absoluteString {
+                Alamofire.request(url).responseImage { response in
+                    //debugPrint(response.result)
+                    if let image = response.result.value {
+                        self.avatarImageView.image = image
                     }
                 }
             }
+            
+            self.title = user.fullName
+            self.nicknameLabel.text = user.username
+            
+            if let posts = user.counts?.media {
+                self.postsLabel.text = "\(posts) posts"
+            }
+            if let followers = user.counts?.followedBy {
+                self.followersLabel.text = "\(followers) followers"
+            }
+            if let following = user.counts?.follows {
+                self.followingLabel.text = "\(following) following"
+            }
+            
+            self.bioLabel.text = user.bio
+            
+            if let website = user.website?.absoluteString {
+                self.websiteLabel.text = website
+            }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
