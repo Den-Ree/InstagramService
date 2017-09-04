14d9b78da2410d83d94ed1b09991728d374fc133
Admin <6753142y@gmail.com>
2017-09-04 16:38:02 +0300

    Minor fix

--- src/InstagramAPI/InstagramAPI/Instagram/Helpers/Instagram+Constants.swift
+++ src/InstagramAPI/InstagramAPI/Instagram/Helpers/Instagram+Constants.swift
@@ -17,9 +17,9 @@ extension Instagram {
   
   enum Constants {
     static let baseUrl = "https://api.instagram.com/"
-    static var appClientId: String = ""
-    static var appClientSecret: String = ""
-    static var appRedirectURL: String = ""
+    static var appClientId: String = " "
+    static var appClientSecret: String = " "
+    static var appRedirectURL: String = " "
     static var baseUrlComponents: URLComponents {
       var components = URLComponents()
       components.host = "api.instagram.com"
